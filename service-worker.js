7675abb1207df7c6cbcc6bf28cf40a2940b89c88
sparcuss2 <sparcuss2@gmail.com>
2025-10-29 17:31:54 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,19 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('cbd-cache-v1').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json'
+      ]);
+    })
+  );
+});
 
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
